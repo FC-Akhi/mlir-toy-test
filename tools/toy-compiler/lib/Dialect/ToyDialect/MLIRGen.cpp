b79fdab2b6de1f0747791e485750879b9154a5c4
FC-Akhi <fahima.chowdhury.173@gmail.com>
2024-03-21 12:40:29 +0600

    comments fix

--- tools/toy-compiler/lib/Dialect/ToyDialect/MLIRGen.cpp
+++ tools/toy-compiler/lib/Dialect/ToyDialect/MLIRGen.cpp
@@ -1,10 +1,10 @@
-///===- MLIRGen.cpp - MLIR Generation(Only a blank module will generate) from a Toy AST -----------------------===///
+///===---------- MLIRGen.cpp - MLIR Generation(Only a blank module will generate) from a Toy AST --------------===///
 ///
 /// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 /// See https:///llvm.org/LICENSE.txt for license information.
 /// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 ///
-///===----------------------------------------------------------------------===///
+///===---------------------------------------------------------------------------------------------------------===///
 ///
 /// This file implements a simple IR generation targeting MLIR from a Module AST
 /// for the Toy language.
